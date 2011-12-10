8e8b2b84988f2e72e2c676e4f4906b03d13266aa
sridharn <sridhar@10gen.com>
2011-12-10 13:58:52 -0800

    BUMP version to 0.5.*

--- ReplicaSets/GlobalAssemblyInfo.cs
+++ ReplicaSets/GlobalAssemblyInfo.cs
@@ -22,4 +22,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.4.0.4361")]
+[assembly: AssemblyVersion("0.5.*")]

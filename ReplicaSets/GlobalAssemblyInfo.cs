fcb12ef198d7582b3d2522167ce999670936a331
sridharn <sridhar@10gen.com>
2011-12-10 13:56:37 -0800

    Set version for tag

--- ReplicaSets/GlobalAssemblyInfo.cs
+++ ReplicaSets/GlobalAssemblyInfo.cs
@@ -21,20 +21,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2010-2011 10gen Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.*")]
+[assembly: AssemblyVersion("0.4.0.4361")]

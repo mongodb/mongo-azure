cf23c053eac84b04b95ea1045e48058f0859ccf6
Phillip Cohen <phillip.cohen@10gen.com>
2012-07-02 14:57:48 -0700

    Extracted.

--- src/MongoDB.WindowsAzure.Manager/Models/ReplicaSetStatus.cs
+++ src/MongoDB.WindowsAzure.Manager/Models/ReplicaSetStatus.cs
@@ -144,16 +144,4 @@ namespace MongoDB.WindowsAzure.Manager.Models
             };
         }
     }
-
-    public class ServerStatus
-    {
-        public Int32 id { get; set; }
-        public string name { get; set; }
-        public string health { get; set; }
-        public Int32 state { get; set; }
-        public string stateStr { get; set; }
-        public string lastHeartbeat { get; set; }
-        public DateTime optimeDate { get; set; }
-        public string pingMS { get; set; }
-    }
 }
---
+++ src/MongoDB.WindowsAzure.Manager/Models/ServerStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoDB.WindowsAzure.Manager.Models
+{
+    /// <summary>
+    /// Represents the status of one MongoDB server.
+    /// </summary>
+    public class ServerStatus
+    {
+        public Int32 id { get; set; }
+        public string name { get; set; }
+        public string health { get; set; }
+        public Int32 state { get; set; }
+        public string stateStr { get; set; }
+        public string lastHeartbeat { get; set; }
+        public DateTime optimeDate { get; set; }
+        public string pingMS { get; set; }
+    }
+}

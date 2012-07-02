8cb39b57279728788fce810dff038185d61ed9b6
Phillip Cohen <phillip.cohen@10gen.com>
2012-07-02 14:35:26 -0700

    Extended the previous solution to also provide dummy data about the servers, so you can build/run/test the app 100% without Azure! Faster development time = better development

--- src/MongoDB.WindowsAzure.Manager/Models/ReplicaSetStatus.cs
+++ src/MongoDB.WindowsAzure.Manager/Models/ReplicaSetStatus.cs
@@ -26,7 +26,7 @@ namespace MongoDB.WindowsAzure.Manager.Models
         public static ReplicaSetStatus GetReplicaSetStatus( )
         {
             if ( Util.IsRunningWebAppDirectly )
-                return new ReplicaSetStatus( "Running web app directly" );
+                return GetDummyStatus( );
 
             ReplicaSetStatus status;
             var settings = ConnectionUtilities.GetConnectionSettings( );
@@ -100,6 +100,48 @@ namespace MongoDB.WindowsAzure.Manager.Models
                 status = new ReplicaSetStatus( "Replica Set Unavailable" );
             }
             return status;
+        }
+
+        /// <summary>
+        /// Returns dummy server information for when the ASP.NET app is being run directly (without Azure).
+        /// </summary>
+        /// <returns></returns>
+        public static ReplicaSetStatus GetDummyStatus( )
+        {
+            return new ReplicaSetStatus( "rs-offline-dummy-data" )
+            {
+                servers = new List<ServerStatus>( new ServerStatus[] { new ServerStatus
+                {
+                    id = 0,
+                    name = "localhost:27018",
+                    health = "UP",
+                    state = 2,
+                    lastHeartbeat = DateTime.Now.Subtract( new TimeSpan( 0, 0, 1 ) ).ToString( "yyyy-MM-dd HH:mm tt" ),
+                    optimeDate = DateTime.Now,
+                    pingMS = new Random( ).Next( 20, 600 ).ToString( ),
+                    stateStr = "SECONDARY"
+                }, new ServerStatus
+                {
+                    id = 1,
+                    name = "localhost:27019",
+                    health = "UP",
+                    state = 1,
+                    lastHeartbeat = "Not Applicable",
+                    optimeDate = DateTime.Now,
+                    pingMS = "Not Applicable",
+                    stateStr = "PRIMARY"
+                }, new ServerStatus
+                {
+                    id = 2,
+                    name = "localhost:27020",
+                    health = "DOWN",
+                    state = 8,
+                    lastHeartbeat = DateTime.MinValue.ToString( "yyyy-MM-dd HH:mm tt" ),
+                    optimeDate = DateTime.MinValue,
+                    pingMS = "0",
+                    stateStr = "(not reachable/healthy)"
+                } } )
+            };
         }
     }
 

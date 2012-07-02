ee07efe5d9ce43daf772c571d7838bd9283a6361
Phillip Cohen <phillip.cohen@10gen.com>
2012-07-02 12:01:38 -0700

    Ka-bam. Dummy content removed.

--- src/MongoDB.WindowsAzure.Manager/Controllers/HomeController.cs
+++ src/MongoDB.WindowsAzure.Manager/Controllers/HomeController.cs
@@ -12,20 +12,6 @@ namespace MongoDB.WindowsAzure.Manager.Controllers
         public ActionResult Index( )
         {         
             return View( ReplicaSetStatus.GetReplicaSetStatus( ) );
-        }
-
-        public ActionResult About( )
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View( );
-        }
-
-        public ActionResult Contact( )
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View( );
-        }
+        }      
     }
 }

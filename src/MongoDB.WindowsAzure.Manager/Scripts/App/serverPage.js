dd90faafb9facb474e2c5eaba2ca6a80d3af6f38
Phillip Cohen <phillip.cohen@10gen.com>
2012-07-03 15:53:57 -0700

    Started to make the server page live.

---
+++ src/MongoDB.WindowsAzure.Manager/Scripts/App/serverPage.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {   
+
+    setInterval( updateTable, 500 );
+});
+
+function updateTable() {
+ $.ajax({
+        url: "/api/server",
+        contentType: "application/json;charset=utf-8",
+        success: function (response) {
+
+            $(".server-row").remove();
+            $.each( response, addToTable );
+        },
+    });
+
+}
+
+function addToTable( index, server ) {
+
+    $("#serversTable").append("<tr class='server-row'>" + 
+        "<td>" + server.Id + "</td>" +
+        "<td>" + server.Name + "</td>" +
+        "<td>" + server.Health + "</td>" +
+        "<td>" + server.CurrentState + "</td>" +
+        "<td>" + server.LastHeartBeat + "</td>" +
+        "<td>" + server.LastOperationTime + "</td>" +
+        "<td>" + server.PingTime + "</td>" +
+    "</tr>");
+
+}

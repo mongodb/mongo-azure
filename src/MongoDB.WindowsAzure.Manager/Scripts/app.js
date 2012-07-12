18fd016cfcdc2a9539ca1e82b15450c59faf6005
Phillip Cohen <phillip.cohen@10gen.com>
2012-07-12 15:26:37 -0700

    Added refresh to log view / added progress indicator.

--- src/MongoDB.WindowsAzure.Manager/Scripts/app.js
+++ src/MongoDB.WindowsAzure.Manager/Scripts/app.js
@@ -1,24 +1,22 @@
 $(document).ready(function () {
 
+    refreshLogFile();
 
+    $("a#refreshLog").click(refreshLogFile); 
+});
+
+/**
+ * Fetches the log file from the server.
+ */
+function refreshLogFile() {
+
+    $("#instanceLog").fadeTo('fast', 0.5);
     $.ajax({ url: '/api/GetServerLogDirect/' + $("#server-id").text(), type: 'GET', success: function (response) {
-      
+
+        $("#logFetchStatus").hide();
+        $("#instanceLog").fadeIn();
+        $("#instanceLog").fadeTo('fast', 1.0);
         $("#instanceLog").html(response.log);
     }
     });
-
-    $("#fetchLogButton").click(function () {
-
-        $("#logArea").fadeIn();
-        $.ajax({ url: '/api/GetServerLogBlob/' + $("#server-id").text(), type: 'GET', success: function (response) {
-
-            $("#instanceLog").hide();
-            $("#instanceLog").html(response.log);
-            $("#instanceLog").fadeIn();
-            $("#logFetchStatus").hide();
-        }
-        });
-
-
-    });
-});
+}

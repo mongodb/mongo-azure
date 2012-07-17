a4638485ae5b7655a1e0abad72d162be98273f93
Phillip Cohen <phillip.cohen@10gen.com>
2012-07-17 11:33:00 -0700

    Added standard Apache header and moved using declarations inside the namespace.

--- src/MongoDB.WindowsAzure.Manager/Src/LogFetcher.cs
+++ src/MongoDB.WindowsAzure.Manager/Src/LogFetcher.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Diagnostics;
-using Microsoft.WindowsAzure.StorageClient;
-using Microsoft.WindowsAzure;
-using System.Text;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using MongoDB.WindowsAzure.Common;
-using System.IO;
+﻿/*
+ * Copyright 2010-2012 10gen Inc.
+ * file : LogFetcher.cs
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 namespace MongoDB.WindowsAzure.Manager.Src
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Diagnostics;
+    using Microsoft.WindowsAzure.StorageClient;
+    using Microsoft.WindowsAzure;
+    using System.Text;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using MongoDB.WindowsAzure.Common;
+    using System.IO;
+
     /// <summary>
     /// Writes the content of MongoDB log files to the HTTP response stream. 
     /// </summary>

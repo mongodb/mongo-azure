265d6b4b5960a7d2bbb3307b428a6c4ff61df3e3
sridharn <sridhar@10gen.com>
2012-07-19 00:16:30 -0700

    Fix AZURE-47. Have WinhostsUpdater create host alias that is prefixed by replica set name

--- src/MongoDB.WindowsAzure.InstanceMaintainer/WinHostsUpdater.cs
+++ src/MongoDB.WindowsAzure.InstanceMaintainer/WinHostsUpdater.cs
@@ -68,7 +68,7 @@ namespace MongoDB.WindowsAzure.InstanceMaintainer
                     configSetter(RoleEnvironment.GetConfigurationSettingValue(configName));
                 });
 
-                var rsName = GetRoleName();
+                var rsName = ConnectionUtilities.GetReplicaSetName();
 
                 Trace.TraceInformation("Replica set name is {0}", rsName);
 
@@ -130,10 +130,6 @@ namespace MongoDB.WindowsAzure.InstanceMaintainer
             return nodes;
         }
 
-        private static string GetRoleName()
-        {
-            return "rs";
-        }
     }
 
 }
